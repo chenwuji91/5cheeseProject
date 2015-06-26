a155b336be1da21cb524df71259a3109c86cc59f
chenwuji <53996386@qq.com>
2015-06-26 14:59:59 +0800

    test file

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,18 @@
+import java.io.File;
+
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		File file=new File("D:\\suzhou");
+		String[] list=file.list();
+		File[] filename=file.listFiles();
+		for(int i=0;i<list.length;i++)
+		System.out.println(filename[i]);
+		
 
-		System.out.println("Hello world");
+		
 	}
 
 }
